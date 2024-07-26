63101361231955f382b39dee072bbf08ea24c697
Dung Paulus CTS <linhtam2001a@gmail.com>
2024-07-26 16:06:30 +0700

    feat:show ui

--- src/app/usermanager/pages/product/product.module.ts
+++ src/app/usermanager/pages/product/product.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProductComponent } from './product.component';
@@ -9,6 +9,9 @@ import { ProductComponent } from './product.component';
     FormsModule,
     CommonModule,
 
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   exports: [ProductComponent],
 })
